572ff1a4e602e63d929663e6cf299ff16ac3a3e7
Michel Moraes <michel.moraes@helloworldlondon.co.uk>
2018-09-09 21:27:27 +0200

    Fixing slick slider responsive

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -13,12 +13,40 @@ new Vue({
     data() {
         return {
             slickOptions: {
-                slidesToShow: 3,
                 dots: true,
                 arrows: false,
                 fade: false,
                 autoplay: true,
-                appendDots: '.slick__dots'
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                infinite: false,
+                appendDots: '.slick__dots',
+                focusOnSelect: false,     
+                swipeToSlide: false,
+                touchThreshold: 50,                
+                responsive: [
+                    {
+                      breakpoint: 1440,
+                      settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                      }
+                    },
+                    {
+                      breakpoint: 1024,
+                      settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                      }
+                    },
+                    {
+                      breakpoint: 480,
+                      settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                      }
+                    }
+                ]                
             },
         };    
     }
