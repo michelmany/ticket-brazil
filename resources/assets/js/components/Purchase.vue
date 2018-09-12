aa9aa7ae0e8fbfdc353bf196217b2d9db57f970e
Michel Moraes <michel.moraes@helloworldlondon.co.uk>
2018-09-12 10:20:50 +0200

    Starting developing Tickets Page and implementing woocommerce

--- resources/assets/js/components/Purchase.vue
+++ resources/assets/js/components/Purchase.vue
@@ -1,80 +1,89 @@
 <template>
-    <section class="purchase">
+    <section class="purchase section">
         <div class="container">
 
-            <div class="purchase__step step-1">
+            <div class="purchase__step step-1" v-show="isStep(1)">
                 <div class="purchase__step-label">
-                    <span>1</span> Select the parade type
+                    <span class="number">1</span> Select the parade type
                 </div>
-                <div class="purchase__items">
-                    <div class="purchase__item button selected">Preliminary Parades</div>
-                    <div class="purchase__item button">Main Parades</div>
-                    <div class="purchase__item button">Champions’ Parade</div>
-                </div>
+                <ul class="purchase__items">
+                    <li class="purchase__item button" @click="nextStep(2, index)" 
+                        :class="{ selected: index === activeItem }" 
+                        v-for="(parade, index) in paradeType" :key="index">{{ parade }}</li>
+                </ul>
             </div>
 
-            <div class="purchase__step step-2">
-                <div class="purchase__step-label">
-                    <span>2</span> Select the date
+            <transition name="fade">
+                <div class="purchase__step step-2" v-show="isStep(2)">
+                    <div class="purchase__step-label">
+                        <span class="number">2</span> Select the date
+                    </div>
+                    <ul class="purchase__items">
+                        <li class="purchase__item button" @click="nextStep(3)">Friday (March 01)</li>
+                        <li class="purchase__item button" @click="nextStep(3)">Saturday (March 02)</li>
+                    </ul>
                 </div>
-                <div class="purchase__items">
-                    <div class="purchase__item button">Friday (March 01)</div>
-                    <div class="purchase__item button">Saturday (March 02)</div>
-                </div>
-            </div>
+            </transition>
 
-            <div class="purchase__step step-3">
-                <div class="purchase__step-label">
-                    <span>3</span> Select the seat type
-                </div>
-                <div class="purchase__items">
-                    <div class="purchase__item button">Grandstand tickets</div>
-                    <div class="purchase__item button">Open front box seats</div>
-                </div>
-            </div>
-
-            <div class="purchase__step step-4">
-                <div class="purchase__step-label">
-                    <span>4</span> Select the sector
-                </div>
-                <div class="purchase__items">
-                    <div class="purchase__item button sector">1</div>
-                    <div class="purchase__item button sector">2</div>
-                    <div class="purchase__item button sector">3</div>
-                    <div class="purchase__item button sector">4</div>
-                    <div class="purchase__item button sector">5</div>
-                    <div class="purchase__item button sector">6</div>
-                    <div class="purchase__item button sector">7</div>
-                    <div class="purchase__item button sector">8</div>
-                    <div class="purchase__item button sector">9</div>
-                    <div class="purchase__item button sector">10</div>
-                </div>
-            </div>
-
-            <div class="purchase__step step-5">
-                <div class="purchase__step-label">
-                    <span>5</span> Enter the quantity
-                </div>
-                <div class="purchase__items">
-                    <div class="purchase__item-quantity">
-                        <button class="button">-</button> 3 <button class="button">+</button>
+            <transition name="fade">
+                <div class="purchase__step step-3" v-show="isStep(3)">
+                    <div class="purchase__step-label">
+                        <span class="number">3</span> Select the seat type
+                    </div>
+                    <div class="purchase__items">
+                        <div class="purchase__item button" @click="nextStep(4)">Grandstand tickets</div>
+                        <div class="purchase__item button" @click="nextStep(4)">Open front box seats</div>
                     </div>
                 </div>
-            </div>
+            </transition>
 
-            <div class="purchase__step step-6">
-                <div class="purchase__step-label">
-                    <span>6</span> Add to cart
+            <transition name="fade">
+                <div class="purchase__step step-4" v-show="isStep(4)">
+                    <div class="purchase__step-label">
+                        <span class="number">4</span> Select the sector
+                    </div>
+                    <div class="purchase__items">
+                        <div class="purchase__item button sector" @click="nextStep(5)">1</div>
+                        <div class="purchase__item button sector" @click="nextStep(5)">2</div>
+                        <div class="purchase__item button sector" @click="nextStep(5)">5</div>
+                        <div class="purchase__item button sector" @click="nextStep(5)">4</div>
+                        <div class="purchase__item button sector" @click="nextStep(5)">5</div>
+                        <div class="purchase__item button sector" @click="nextStep(5)">6</div>
+                        <div class="purchase__item button sector" @click="nextStep(5)">7</div>
+                        <div class="purchase__item button sector" @click="nextStep(5)">8</div>
+                        <div class="purchase__item button sector" @click="nextStep(5)">9</div>
+                        <div class="purchase__item button sector" @click="nextStep(5)">10</div>
+                    </div>
                 </div>
-                <div class="purchase__item-total">
-                    <span>3x $18.40</span>
+            </transition>
 
-                    <button class="purchase__add-to-cart-btn button">
-                        Add to cart
-                    </button>
+            <transition name="fade">
+                <div class="purchase__step step-5" v-show="isStep(5)">
+                    <div class="purchase__step-label">
+                        <span class="number">5</span> Enter the quantity
+                    </div>
+                    <div class="purchase__items">
+                        <div class="purchase__item-quantity">
+                            <button class="button">-</button> 3 <button class="button">+</button>
+                        </div>
+                    </div>
                 </div>
+            </transition>
 
-            </div>             
+            <transition name="fade">
+                <div class="purchase__step step-6" v-show="isStep(5)">
+                    <div class="purchase__step-label">
+                        <span class="number">6</span> Add to cart
+                    </div>
+                    <div class="purchase__item-total">
+                        <span>3x $18.40</span>
+
+                        <button class="purchase__add-to-cart-btn button">
+                            Add to cart
+                        </button>
+                    </div>
+                </div>             
+            </transition>
 
         </div>  
     </section>
@@ -82,10 +91,44 @@
 
 <script>
     export default {
+        data() {
+            return {
+                currentStep: 1,
+                paradeType: ['Preliminary Parades', 'Main Parades', 'Champions’ Parade'],
+                activeItem: -1
+            }
+        },
+
+        computed: {
+            
+        },
+
+        methods: {
+            nextStep(val, idx) {
+                this.currentStep = val
+                let selected = event.target
+                selected.classList.add('selected')
+                this.activeItem = idx
+            },
+
+            isStep(val) {
+                return this.currentStep >= val
+            }
+        },
+
+        mounted() {
+            console.log(this.currentStep);
+        }
         
     }
 </script>
 
-<style scoped>
+<style lang="sass">
+
+    .fade-enter-active, .fade-leave-active
+        transition: opacity .5s;
+
+    .fade-enter, .fade-leave-to
+        opacity: 0
 
 </style>
