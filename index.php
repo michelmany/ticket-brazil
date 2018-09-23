dec7856c64fc28e3ab26cdb9d4a033d879627ff5
Michel Moraes <michel.moraes@helloworldlondon.co.uk>
2018-09-24 00:13:34 +0200

    Fixing Header title and adding latest articles widget links

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 
 use Basecamp\Models\Post;
 
-$context                = Timber::get_context();
-$context['post']        = new TimberPost();
-$context['posts']       = new Timber\PostQuery();
-$context['pagination']  = Timber::get_pagination($pagination_mid_size);
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+$context['posts'] = new Timber\PostQuery();
+$context['pagination'] = Timber::get_pagination($pagination_mid_size);
 
 Timber::render('index.twig', $context);
