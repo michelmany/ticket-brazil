7efad1f2c08527e8604528be01b0a6be867a787b
Michel Moraes <michel.moraes@helloworldlondon.co.uk>
2018-10-25 15:26:50 +0200

    Styling contact form page

--- template-contact.php
+++ template-contact.php
@@ -8,5 +8,7 @@ $post            = new TimberPost();
 $context['post'] = $post;
 
 $context['contact_shortcode'] = do_shortcode(get_field('contact_form_shortcode'));
+$context['security_warning_image'] = get_field('security_warning_image');
+$context['security_warning_text'] = get_field('security_warning_text');
 
 Timber::render('template-contact.twig', $context);
